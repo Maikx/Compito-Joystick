d652cf23c1451a90836e5049542679c8dd30f7c7
Maikx <51028649+Maikx@users.noreply.github.com>
2020-12-30 11:30:03 +0100

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,256 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Player.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Player : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Player()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Player"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerMain"",
+            ""id"": ""43ca38ec-c7fd-45f3-94d5-af1ef2883c3e"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""c77e5a55-8b65-403e-be33-98eaea3e3a2f"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Use"",
+                    ""type"": ""Button"",
+                    ""id"": ""e737e03e-899e-447f-ba0f-3048a02a6b1d"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Value"",
+                    ""id"": ""e4bf2c66-32ce-453c-99dc-5a0ca8bb5008"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""18d8c68e-d12e-4f5a-a165-bd70e0db701f"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""902882a9-6c06-4702-a702-70e555c20deb"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""bdb01c34-890e-45f3-b7eb-d894e9de8aff"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""fd02d841-b076-4f4e-bdec-20193c8c2b3f"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""eef38b8f-35a6-4cd8-87b2-8ca2a5014e55"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""cd77fd55-67f1-4c33-be30-1b313a4673ee"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2b780995-ec27-42c7-9f24-c5b41de3913f"",
+                    ""path"": ""<Mouse>/press"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Use"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c701b569-e4b6-4d56-aa5b-9370badeb28f"",
+                    ""path"": ""<Touchscreen>/press"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Use"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""00a32db4-10a6-4ad3-aae3-5baf8020c733"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerMain
+        m_PlayerMain = asset.FindActionMap("PlayerMain", throwIfNotFound: true);
+        m_PlayerMain_Move = m_PlayerMain.FindAction("Move", throwIfNotFound: true);
+        m_PlayerMain_Use = m_PlayerMain.FindAction("Use", throwIfNotFound: true);
+        m_PlayerMain_Look = m_PlayerMain.FindAction("Look", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerMain
+    private readonly InputActionMap m_PlayerMain;
+    private IPlayerMainActions m_PlayerMainActionsCallbackInterface;
+    private readonly InputAction m_PlayerMain_Move;
+    private readonly InputAction m_PlayerMain_Use;
+    private readonly InputAction m_PlayerMain_Look;
+    public struct PlayerMainActions
+    {
+        private @Player m_Wrapper;
+        public PlayerMainActions(@Player wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_PlayerMain_Move;
+        public InputAction @Use => m_Wrapper.m_PlayerMain_Use;
+        public InputAction @Look => m_Wrapper.m_PlayerMain_Look;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerMain; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerMainActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerMainActions instance)
+        {
+            if (m_Wrapper.m_PlayerMainActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Use.started -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnUse;
+                @Use.performed -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnUse;
+                @Use.canceled -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnUse;
+                @Look.started -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnLook;
+                @Look.performed -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnLook;
+                @Look.canceled -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnLook;
+            }
+            m_Wrapper.m_PlayerMainActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Use.started += instance.OnUse;
+                @Use.performed += instance.OnUse;
+                @Use.canceled += instance.OnUse;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
+            }
+        }
+    }
+    public PlayerMainActions @PlayerMain => new PlayerMainActions(this);
+    public interface IPlayerMainActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnUse(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float playerSpeed = 2.0f;
+
+    public float jumpHeight = 1.0f;
+
+    public float gravityValue = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+    private Player playerInput;
+
+    private void Awake()
+    {
+        playerInput = new Player();
+    }
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movementInput = playerInput.PlayerMain.Move.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movementInput.x, 0f, movementInput.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Puzzle15")
+        {
+            if (playerInput.PlayerMain.Use.triggered)
+            {
+                SceneManager.LoadScene("Puzzle 15");
+            }
+        }
+
+        if (other.gameObject.name == "Differences")
+        {
+            if (playerInput.PlayerMain.Use.triggered)
+            {
+                SceneManager.LoadScene("Differences");
+            }
+        }
+
+        if (other.gameObject.name == "Intruder")
+        {
+            if (playerInput.PlayerMain.Use.triggered)
+            {
+                SceneManager.LoadScene("Intruder");
+            }
+        }
+    }
+}
